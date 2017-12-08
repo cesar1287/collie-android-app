1d7290b656a0a169f613e4377f465aa64c42d180
Islane <islane.junior@gmail.com>
2017-12-08 17:28:17 -0300

    Finalizando o layout da tela forgot_password_activity

--- app/src/main/java/comcesar1287/github/www/collie/view/ForgotPasswordActivity.java
+++ app/src/main/java/comcesar1287/github/www/collie/view/ForgotPasswordActivity.java
@@ -3,6 +3,8 @@ package comcesar1287.github.www.collie.view;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+import comcesar1287.github.www.collie.R;
+
 public class ForgotPasswordActivity extends AppCompatActivity {
 
     @Override
