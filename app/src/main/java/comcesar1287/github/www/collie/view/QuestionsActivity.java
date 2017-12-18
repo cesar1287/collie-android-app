b37dd902d49c0260165d8487de13474b330564fe
Islane <islane.junior@gmail.com>
2017-12-18 09:52:35 -0300

    Implementando contandor regressivo da tela de perguntas e respostas

--- app/src/main/java/comcesar1287/github/www/collie/view/QuestionsActivity.java
+++ app/src/main/java/comcesar1287/github/www/collie/view/QuestionsActivity.java
@@ -1,15 +1,40 @@
 package comcesar1287.github.www.collie.view;
 
+import android.os.CountDownTimer;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 import comcesar1287.github.www.collie.R;
 
 public class QuestionsActivity extends AppCompatActivity {
 
+    TextView time_regressive;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_questions);
+
+        initComponents();
+        countdown();
+    }
+
+    private void initComponents() {
+        time_regressive = (TextView) findViewById(R.id.question_time);
+    }
+
+    private void countdown(){
+        new CountDownTimer(20000, 1000) {
+
+            public void onTick(long millisUntilFinished) {
+                time_regressive.setText(""+millisUntilFinished / 1000);
+            }
+
+            public void onFinish() {
+
+            }
+        }.start();
     }
 }
