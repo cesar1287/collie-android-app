c43495d0217e2fc247312a6c1745631c0d1560e2
Islane <islane.junior@gmail.com>
2017-12-19 10:50:27 -0300

    Criando activitys para configurações de bloqueios

---
+++ app/src/main/java/comcesar1287/github/www/collie/view/ConfigBlockPointsActivity.java
@@ -0,0 +1,15 @@
+package comcesar1287.github.www.collie.view;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import comcesar1287.github.www.collie.R;
+
+public class ConfigBlockPointsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config_block_points);
+    }
+}
