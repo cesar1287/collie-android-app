8fbffc1b31ce03846e13e22f9e69a85173e9d934
César Rodrigues <cesar.nascimento.ufc@gmail.com>
2017-12-19 16:43:26 -0300

    Implementação inicial da tela de configuração simples

--- app/src/main/java/comcesar1287/github/www/collie/view/ConfigBlockSimpleActivity.java
+++ app/src/main/java/comcesar1287/github/www/collie/view/ConfigBlockSimpleActivity.java
@@ -2,14 +2,42 @@ package comcesar1287.github.www.collie.view;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
 
 import comcesar1287.github.www.collie.R;
 
-public class ConfigBlockSimpleActivity extends AppCompatActivity {
+public class ConfigBlockSimpleActivity extends AppCompatActivity implements View.OnClickListener{
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_config_block_simple);
+
+        initComponents();
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        switch (id){
+            case R.id.config_simple_button_send:
+                Toast.makeText(this, "Configuração de bloqueio registrado com sucesso", Toast.LENGTH_SHORT).show();
+                finish();
+                break;
+            case R.id.config_simple_button_cancel:
+                finish();
+                break;
+        }
+    }
+
+    private void initComponents() {
+        Button btSend = findViewById(R.id.config_simple_button_send);
+        btSend.setOnClickListener(this);
+
+        Button btCancel = findViewById(R.id.config_simple_button_cancel);
+        btCancel.setOnClickListener(this);
     }
 }
