ea9e4b7dcd3f61c5063c22b0f26a5bb5fa93c81f
Islane <islane.junior@gmail.com>
2018-01-02 10:43:52 -0300

    Criando as telas de instruções

---
+++ app/src/main/java/comcesar1287/github/www/collie/view/InstructionSlideActivity.java
@@ -0,0 +1,26 @@
+package comcesar1287.github.www.collie.view;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import comcesar1287.github.www.collie.R;
+
+public class InstructionSlideActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.instruction_slide_one);
+
+        initToolbar();
+    }
+
+    private void initToolbar() {
+        ActionBar actionBar = getSupportActionBar();
+
+        if(actionBar!=null) {
+            actionBar.hide();
+        }
+    }
+}
