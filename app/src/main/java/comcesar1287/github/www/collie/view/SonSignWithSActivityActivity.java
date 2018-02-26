da784a237bd3870bb4dd4ee62443d00aaeb751d5
Islane <islane.junior@gmail.com>
2018-02-26 11:14:12 -0300

    Mudando tela de login do dependente

--- app/src/main/java/comcesar1287/github/www/collie/view/SonSignWithSActivityActivity.java
+++ app/src/main/java/comcesar1287/github/www/collie/view/SonSignWithSActivityActivity.java
@@ -34,7 +34,7 @@ import comcesar1287.github.www.collie.controller.data.SharedPref;
 
 public class SonSignWithSActivityActivity extends AppCompatActivity implements View.OnClickListener{
 
-    private TextInputLayout etEmail;
+    private TextInputLayout etPasswordPhone;
 
     private FirebaseAuth mAuth;
 
@@ -47,7 +47,7 @@ public class SonSignWithSActivityActivity extends AppCompatActivity implements V
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_son_sign_with_sactivity);
+        setContentView(R.layout.activity_register_phone_son);
 
         initToolbar();
         initComponents();
@@ -59,7 +59,7 @@ public class SonSignWithSActivityActivity extends AppCompatActivity implements V
 
         switch (id){
             case R.id.son_sign_with_btn_send:
-                //attemptLogin();
+                attemptLogin();
                 break;
         }
     }
@@ -84,109 +84,109 @@ public class SonSignWithSActivityActivity extends AppCompatActivity implements V
         mAuth = FirebaseAuth.getInstance();
         Utils.init(getApplication());
 
-        etEmail = findViewById(R.id.son_sign_with_email);;
+        etPasswordPhone = findViewById(R.id.son_sign_with_edit_phone);
 
-        Button btContinue = findViewById(R.id.son_sign_with_btn_send);
+        Button btContinue = findViewById(R.id.sign_with_btn_send);
         btContinue.setOnClickListener(this);
-
 
     }
 
-//    private void attemptLogin() {
-//        String email = etEmail.getEditText().getText().toString();
-//
-//        boolean allFieldsFilled = true;
-//
-//        if (EmptyUtils.isEmpty(email)) {
-//            allFieldsFilled = false;
-//            etEmail.setError(getString(R.string.error_required_field));
-//        }else{
-//            etEmail.setErrorEnabled(false);
-//        }
-//
-//        if (allFieldsFilled) {
-//            if(NetworkUtils.isConnected()) {
-//                makeLoginRequest(email);
-//            }else{
-//                Toast.makeText(this, "Falha ao entrar, sem conexão com a internet", Toast.LENGTH_SHORT).show();
-//            }
-//        }
-//    }
-//
-//    private void makeLoginRequest(String email) {
-//        dialog = ProgressDialog.show(SonSignWithSActivityActivity.this,"",
-//                SonSignWithSActivityActivity.this.getResources().getString(R.string.processing_login), true, false);
-//        mAuth.signInWithEmailAndPassword(email)
-//                .addOnFailureListener(SonSignWithSActivityActivity.this, new OnFailureListener() {
-//                    @Override
-//                    public void onFailure(@NonNull Exception e) {
-//                        dialog.dismiss();
-//                        if(e instanceof FirebaseAuthInvalidUserException){
-//                            Toast.makeText(SonSignWithSActivityActivity.this,
-//                                    getResources().getString(R.string.error_user_password_incorrect),
-//                                    Toast.LENGTH_SHORT).show();
-//                        }else if(e instanceof FirebaseAuthInvalidCredentialsException) {
-//                            Toast.makeText(SonSignWithSActivityActivity.this,
-//                                    getResources().getString(R.string.error_user_password_incorrect),
-//                                    Toast.LENGTH_SHORT).show();
-//                        }else{
-//                            Toast.makeText(SonSignWithSActivityActivity.this,
-//                                    getResources().getString(R.string.error_unknown_error),
-//                                    Toast.LENGTH_SHORT).show();
-//                        }
-//                    }
-//                })
-//                .addOnCompleteListener(SonSignWithSActivityActivity.this, new OnCompleteListener<AuthResult>() {
-//                    @Override
-//                    public void onComplete(@NonNull Task<AuthResult> task) {
-//
-//                        // If sign in fails, display a message to the user. If sign in succeeds
-//                        // the auth state listener will be notified and logic to handle the
-//                        // signed in user can be handled in the listener.
-//                        dialog.dismiss();
-//                        if (task.isSuccessful()) {
-//                            mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
-//                                @Override
-//                                public void onDataChange(DataSnapshot dataSnapshot) {
-//                                    if (dataSnapshot.child("users")
-//                                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-//                                            .child("block")
-//                                            .exists()) {
-//                                        SharedPref sharedPref = new SharedPref(SonSignWithSActivityActivity.this);
-//                                        sharedPref.setTypeBlock((String) dataSnapshot.child("users")
-//                                                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
-//                                                .child("block").getValue());
-//
-//                                        Intent i = new Intent(SonSignWithSActivityActivity.this, MainActivity.class);
-//                                        prefs = getSharedPreferences("preferencias", Context.MODE_PRIVATE);
-//                                        SharedPreferences.Editor ed = prefs.edit();
-//                                        ed.putString("key", msg());
-//                                        ed.apply();
-//                                        startActivity(i);
-//                                        finish();
-//
-//                                    } else {
-//                                        Intent i = new Intent(SonSignWithSActivityActivity.this, SetupScreenActivity.class);
-//                                        prefs = getSharedPreferences("preferencias", Context.MODE_PRIVATE);
-//                                        SharedPreferences.Editor ed = prefs.edit();
-//                                        ed.putString("key", msg());
-//                                        ed.apply();
-//                                        startActivity(i);
-//                                        finish();
-//                                    }
-//                                }
-//
-//                                @Override
-//                                public void onCancelled(DatabaseError databaseError) {
-//                                    Toast.makeText(SonSignWithSActivityActivity.this,
-//                                            getResources().getString(R.string.error_unknown_error),
-//                                            Toast.LENGTH_SHORT).show();
-//                                }
-//                            });
-//                        }
-//                    }
-//                });
-//    }
+    private void attemptLogin() {
+        String password = etPasswordPhone.getEditText().getText().toString();
+
+        boolean allFieldsFilled = true;
+
+
+        if (EmptyUtils.isEmpty(password)) {
+            allFieldsFilled = false;
+            etPasswordPhone.setError(getString(R.string.error_required_field));
+        }else{
+            etPasswordPhone.setErrorEnabled(false);
+        }
+
+        if (allFieldsFilled) {
+            if(NetworkUtils.isConnected()) {
+                makeLoginRequest(password);
+            }else{
+                Toast.makeText(this, "Falha ao entrar, sem conexão com a internet", Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void makeLoginRequest(String email, String password) {
+        dialog = ProgressDialog.show(SonSignWithSActivityActivity.this,"",
+                SonSignWithSActivityActivity.this.getResources().getString(R.string.processing_login), true, false);
+        mAuth.signInWithEmailAndPassword(email, password)
+                .addOnFailureListener(SonSignWithSActivityActivity.this, new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        dialog.dismiss();
+                        if(e instanceof FirebaseAuthInvalidUserException){
+                            Toast.makeText(SonSignWithSActivityActivity.this,
+                                    getResources().getString(R.string.error_user_password_incorrect),
+                                    Toast.LENGTH_SHORT).show();
+                        }else if(e instanceof FirebaseAuthInvalidCredentialsException) {
+                            Toast.makeText(SonSignWithSActivityActivity.this,
+                                    getResources().getString(R.string.error_user_password_incorrect),
+                                    Toast.LENGTH_SHORT).show();
+                        }else{
+                            Toast.makeText(SonSignWithSActivityActivity.this,
+                                    getResources().getString(R.string.error_unknown_error),
+                                    Toast.LENGTH_SHORT).show();
+                        }
+                    }
+                })
+                .addOnCompleteListener(SonSignWithSActivityActivity.this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+
+                        // If sign in fails, display a message to the user. If sign in succeeds
+                        // the auth state listener will be notified and logic to handle the
+                        // signed in user can be handled in the listener.
+                        dialog.dismiss();
+                        if (task.isSuccessful()) {
+                            mDatabase.addListenerForSingleValueEvent(new ValueEventListener() {
+                                @Override
+                                public void onDataChange(DataSnapshot dataSnapshot) {
+                                    if (dataSnapshot.child("users")
+                                            .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                            .child("block")
+                                            .exists()) {
+                                        SharedPref sharedPref = new SharedPref(SonSignWithSActivityActivity.this);
+                                        sharedPref.setTypeBlock((String) dataSnapshot.child("users")
+                                                .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                                .child("block").getValue());
+
+                                        Intent i = new Intent(SonSignWithSActivityActivity.this, MainActivity.class);
+                                        prefs = getSharedPreferences("preferencias", Context.MODE_PRIVATE);
+                                        SharedPreferences.Editor ed = prefs.edit();
+                                        ed.putString("key", msg());
+                                        ed.apply();
+                                        startActivity(i);
+                                        finish();
+
+                                    } else {
+                                        Intent i = new Intent(SonSignWithSActivityActivity.this, SetupScreenActivity.class);
+                                        prefs = getSharedPreferences("preferencias", Context.MODE_PRIVATE);
+                                        SharedPreferences.Editor ed = prefs.edit();
+                                        ed.putString("key", msg());
+                                        ed.apply();
+                                        startActivity(i);
+                                        finish();
+                                    }
+                                }
+
+                                @Override
+                                public void onCancelled(DatabaseError databaseError) {
+                                    Toast.makeText(SonSignWithSActivityActivity.this,
+                                            getResources().getString(R.string.error_unknown_error),
+                                            Toast.LENGTH_SHORT).show();
+                                }
+                            });
+                        }
+                    }
+                });
+    }
 
     public String msg(){
         String user;
@@ -196,6 +196,4 @@ public class SonSignWithSActivityActivity extends AppCompatActivity implements V
 
         return user;
     }
-
-
 }
